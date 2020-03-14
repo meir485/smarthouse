a921c19ee9da318bce35fcacbb55b23e218d0975
meir485 <meirsimchaleibler@gmail.com>
2020-03-14 19:25:48 +0200

    fixed gh-pages

---
+++ build/precache-manifest.b59ec5e98914599a8eb483a916afbe32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8621477ff2fc2174844e9824cd37d69c",
+    "url": "/meir485/smarthouse/index.html"
+  },
+  {
+    "revision": "4fd073e8bebbb8d34035",
+    "url": "/meir485/smarthouse/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b6df4d2507f13d24a286",
+    "url": "/meir485/smarthouse/static/css/main.3658aed0.chunk.css"
+  },
+  {
+    "revision": "4fd073e8bebbb8d34035",
+    "url": "/meir485/smarthouse/static/js/2.0b08c318.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/meir485/smarthouse/static/js/2.0b08c318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6df4d2507f13d24a286",
+    "url": "/meir485/smarthouse/static/js/main.a14e148f.chunk.js"
+  },
+  {
+    "revision": "de8e72cb1d171d585f31",
+    "url": "/meir485/smarthouse/static/js/runtime-main.9dce8651.js"
+  }
+]);
